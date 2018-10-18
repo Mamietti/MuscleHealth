dd17c6524d7682dc7fbccbde38e0140a2132544e
mamietti <mauri.miettinen13@gmail.com>
2018-10-18 17:31:45 +0300

    Moved from Myo SDK to myolib due to inadequate functionality

--- app/src/main/java/com/example/mauri/musclehealth/MuscleHealthActivity.java
+++ app/src/main/java/com/example/mauri/musclehealth/MuscleHealthActivity.java
@@ -7,10 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.thalmic.myo.Hub;
+import eu.darken.myolib.Myo;
+import eu.darken.myolib.MyoConnector;
+import eu.darken.myolib.processor.emg.EmgData;
+import eu.darken.myolib.processor.emg.EmgProcessor;
 
-import static android.content.ContentValues.TAG;
-
+import java.util.Arrays;
+import java.util.List;
 
 public class MuscleHealthActivity extends Activity {
 
@@ -18,18 +21,28 @@ public class MuscleHealthActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muscle_health);
-        Button button = (Button) findViewById(R.id.stupidButton);
+        Button button = findViewById(R.id.stupidButton);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                TextView text = (TextView) findViewById(R.id.helloTextView);
+                TextView text = findViewById(R.id.helloTextView);
                 text.setText(getString(R.string.test_text));
             }
         });
-        Hub hub = Hub.getInstance();
-        if (!hub.init(this)) {
-            Log.e(TAG, "Could not initialize the Hub.");
-            finish();
-            return;
-        }
+        MyoConnector connector = new MyoConnector(getApplicationContext());
+        connector.scan(5000, new MyoConnector.ScannerCallback(){
+            @Override
+            public void onScanFinished(List<Myo> myos) {
+                Myo myo = myos.get(0);
+                myo.connect();
+                EmgProcessor emgProcessor = new EmgProcessor();
+                myo.addProcessor(emgProcessor);
+                emgProcessor.addListener(new EmgProcessor.EmgDataListener() {
+                    @Override
+                    public void onNewEmgData(EmgData emgData) {
+                        Log.i("EMG-Data", Arrays.toString(emgData.getData()));
+                    }
+                });
+            }
+        });
     }
 }
