89e68ce57f679980203399e6c6d2c084a039467b
mamietti <mauri.miettinen13@gmail.com>
2018-11-07 23:07:17 +0200

    Added functional BlueTooth listener setup to software, converted button (temporarily?) into Connect/Disconnect

--- app/src/main/java/com/example/mauri/musclehealth/MuscleHealthActivity.java
+++ app/src/main/java/com/example/mauri/musclehealth/MuscleHealthActivity.java
@@ -1,74 +1,103 @@
 package com.example.mauri.musclehealth;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Set;
+
 import eu.darken.myolib.Myo;
-import eu.darken.myolib.MyoConnector;
 import eu.darken.myolib.processor.emg.EmgData;
+import eu.darken.myolib.MyoCmds;
 import eu.darken.myolib.processor.emg.EmgProcessor;
 
-import java.util.Arrays;
-import java.util.List;
+public class MuscleHealthActivity extends Activity implements EmgProcessor.EmgDataListener, View.OnClickListener{
 
-public class MuscleHealthActivity extends Activity {
+    private ArrayList<Long> dataTimes;
+    private ArrayList<String> dataPoints;
+
+    private Myo mMyo;
+    private boolean mMyoConnected = false;
+    private EmgProcessor emgProcessor = new EmgProcessor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_muscle_health);
+
         Button button = findViewById(R.id.stupidButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                TextView text = findViewById(R.id.helloTextView);
-                text.setText(getString(R.string.test_text));
-            }
-        });
-        MyoConnector connector = new MyoConnector(getApplicationContext());
-        connector.scan(5000, new MyoConnector.ScannerCallback(){
-            @Override
-            public void onScanFinished(List<Myo> myos) {
-                Myo myo = myos.get(0);
-                myo.connect();
-                EmgProcessor emgProcessor = new EmgProcessor();
-                myo.addProcessor(emgProcessor);
-                emgProcessor.addListener(new EmgProcessor.EmgDataListener() {
-                    @Override
-                    public void onNewEmgData(EmgData emgData) {
-                        Log.i("EMG-Data", Arrays.toString(emgData.getData()));
-                    }
-                });
-            }
-        });
-        Handler handler = new Handler();
-        private Runnable runnableCode = new Runnable() {
-            @Override
-            public void run() {
-                Log.d("Handlers", "Called on main thread");
-                handler.postDelayed(this, 2000);
-            }
-        };
-        handler.post(runnableCode);
+        button.setOnClickListener(this);
+
     }
     @Override
     public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
         super.onSaveInstanceState(outState, outPersistentState);
-
-        //outState.putString("theWord", theWord); // Saving the Variable theWord
-        //outState.putStringArrayList("fiveDefns", fiveDefns); // Saving the ArrayList fiveDefns
+        outState.putSerializable("dataTimes", dataTimes.toArray(new Long[dataTimes.size()]));
+        outState.putSerializable("dataPoints", dataPoints.toArray(new String[dataPoints.size()]));
     }
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onRestoreInstanceState(savedInstanceState, persistentState);
+        dataTimes = new ArrayList<>(Arrays.asList((Long[])savedInstanceState.getSerializable("dataTimes")));
+        dataPoints = new ArrayList<>(Arrays.asList((String[])savedInstanceState.getSerializable("dataPoints")));
+    }
 
-        //theWord = savedInstanceState.getString("theWord"); // Restoring theWord
-        //fiveDefns = savedInstanceState.getStringArrayList("fiveDefns"); //Restoring fiveDefns
+    private long mLastEmgUpdate = 0;
+
+    @Override
+    public void onNewEmgData(final EmgData emgData) {
+        if (System.currentTimeMillis() - mLastEmgUpdate > 500) {
+            Log.d("MLog: DATA", emgData.toString());
+            mLastEmgUpdate = System.currentTimeMillis();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView text = findViewById(R.id.helloTextView);
+        if (mMyoConnected){
+            text.setText(getString(R.string.disconnected_text));
+            mMyo.writeMode(MyoCmds.EmgMode.NONE, MyoCmds.ImuMode.ALL, MyoCmds.ClassifierMode.DISABLED, null);
+            emgProcessor.removeDataListener(this);
+            mMyo.removeProcessor(emgProcessor);
+            mMyo.writeSleepMode(MyoCmds.SleepMode.NORMAL, null);
+            mMyo.disconnect();
+            mMyo = null;
+            mMyoConnected = false;
+        } else {
+            text.setText(getString(R.string.connected_text));
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            if (pairedDevices.size() == 1) {
+                for (BluetoothDevice device : pairedDevices) {
+                    Log.d("MLog: Connected", device.getName());
+                    mMyo = new Myo(getApplicationContext(), device);
+                    mMyo.connect();
+                    mMyo.writeMode(MyoCmds.EmgMode.RAW, MyoCmds.ImuMode.NONE, MyoCmds.ClassifierMode.DISABLED, null);
+                    mMyo.writeSleepMode(MyoCmds.SleepMode.NEVER, null);
+                    mMyo.addProcessor(emgProcessor);
+                    emgProcessor.addListener(this);
+                }
+            }
+            mMyoConnected = true;
+        }
+
+    }
+
+    public Myo getMyo() {
+        return mMyo;
+    }
+
+    public void setMyo(Myo myo) {
+        mMyo = myo;
     }
 }
